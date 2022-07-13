b19bd0d6976871c78f4d918d110d50c05be68b1b
Princes61 <107455955+Princes61@users.noreply.github.com>
2022-07-13 18:46:21 +0530

    Add files via upload

---
+++ main/plugins/start.py
@@ -0,0 +1,79 @@
+#  This file is part of the VIDEOconvertor distribution.
+#  Copyright (c) 2021 vasusen-code ; All rights reserved. 
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, version 3.
+#
+#  This program is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#  General Public License for more details.
+#
+#  License can be found in < https://github.com/vasusen-code/VIDEOconvertor/blob/public/LICENSE> .
+
+from telethon import events, Button
+from ethon.teleutils import mention
+from ethon.mystarts import vc_menu
+
+from .. import Drone, ACCESS_CHANNEL, AUTH_USERS
+
+from main.plugins.actions import set_thumbnail, rem_thumbnail, heroku_restart
+from LOCAL.localisation import START_TEXT as st
+from LOCAL.localisation import info_text, spam_notice, help_text, DEV, source_text, SUPPORT_LINK
+
+@Drone.on(events.NewMessage(incoming=True, pattern="/start"))
+async def start(event):
+    await event.reply(f'{st}', 
+                      buttons=[
+                              [Button.inline("Menu.", data="menu")]
+                              ])
+    tag = f'[{event.sender.first_name}](tg://user?id={event.sender_id})'
+    await Drone.send_message(int(ACCESS_CHANNEL), f'{tag} started the BOT')
+                    
+@Drone.on(events.callbackquery.CallbackQuery(data="menu"))
+async def menu(event):
+    await event.edit('**MENU**',
+                    buttons=[[
+                         Button.inline("SET THUMB", data="sett"),
+                         Button.inline("REM THUMB", data='remt')],
+                         [
+                         Button.url("HELP", url="https://telegra.ph/Helps-07-04"),
+                         Button.url("SUPPORT", url=f"{SUPPORT_LINK}")]])
+                   
+ #-----------------------------------------------------------------------------------------------                            
+    
+@Drone.on(events.callbackquery.CallbackQuery(data="sett"))
+async def sett(event):    
+    button = await event.get_message()
+    msg = await button.get_reply_message() 
+    await event.delete()
+    async with Drone.conversation(event.chat_id) as conv: 
+        xx = await conv.send_message("Send me any image for thumbnail as a `reply` to this message.")
+        x = await conv.get_response()
+        if not x.media:
+            xx.edit("No media found.")
+        mime = x.file.mime_type
+        if not 'png' in mime:
+            if not 'jpg' in mime:
+                if not 'jpeg' in mime:
+                    return await xx.edit("No image found.")
+        await set_thumbnail(event, x.media)
+        await xx.delete()
+        
+@Drone.on(events.callbackquery.CallbackQuery(data="remt"))
+async def remt(event):  
+    await event.delete()
+    await rem_thumbnail(event)
+    
+@Drone.on(events.callbackquery.CallbackQuery(data="restart"))
+async def res(event):
+    if not f'{event.sender_id}' == f'{int(AUTH_USERS)}':
+        return await event.edit("Only authorized user can restart!")
+    result = await heroku_restart()
+    if result is None:
+        await event.edit("You have not filled `HEROKU_API` and `HEROKU_APP_NAME` vars.")
+    elif result is False:
+        await event.edit("An error occured!")
+    elif result is True:
+        await event.edit("Restarting app, wait for a minute.")
